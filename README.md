94a62045c92b373120d33146245098b8891f1b3d
Michael Stallings <stall84@gmail.com>
2020-04-09 12:45:48 -0400

    wired up the first 1/5th of project. added README file

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple, student-educational based API that stores, updates, and displays owners of pets, and their specific pets information.  A bit more work involved in that the objects are more complex/nested.
+Eventually want to wire up a webpage interface for communicating with the Express built back-end which is the only part of the initial package.
+nodemon is listed in dependencies but is not necessary for the final package.
--- pets-api.js
+++ pets-api.js
@@ -8,4 +8,61 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// PORT initialization
+const PORT = process.env.PORT || 4005;
 
+// create our (global) array of owner/pet objects
+var owners = [
+    { id: 1,
+        name: "Adam",
+        pets: [
+            {
+                id: 1,
+                name: "Vera",
+                type: "Dog"
+            },
+            {
+                id: 2,
+                name: "Felix",
+                type: "Cat"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Kamilah",
+        pets: [
+            {
+                id: 1,
+                name: "Doug",
+                type: "Dog"
+            }
+        ]
+    }
+];
+
+
+// Initializer/Listener
+app.listen(PORT, () => {
+    console.log(`Pets-API server is running/listening on PORT: ${PORT}`);
+})
+
+// GET /api/owners
+app.get('/api/owners', (req,res) => {
+    res.send(owners);
+})
+
+// GET /api/owners/:id
+// route to get data for owners by id #
+app.get('/api/owners/:id', (req,res) => {
+    // logic to match the requested ID. Use parseInt method to turn the string returned to an integer
+    let ownerID = parseInt(req.params.id);
+    let currID = owners.find((element) => {
+        return element.id === ownerID;
+    });
+    // add a 404 response if the ID parameter is not found in the global owners variable
+    if (!currID) {
+        res.status(404).send('The owner ID specified was not found (404)');
+    };
+    res.send(currID);
+})
